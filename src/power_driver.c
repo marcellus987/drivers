b091d457ec53bd0d2e20d76b0ba9d25df7bba76c
Marcellus <marcellus.sacramento@gmail.com>
2026-07-15 01:31:26 -0700

    Modified standby mode for wake up source sselection.

--- src/power_driver.c
+++ src/power_driver.c
@@ -4,7 +4,7 @@
 static void clear_exti_lines(void) {
 	/* Ensure no EXTI line is pending. */
 	EXTI->PR |= 0x67FFFFU;
-}
+} /* End of clear_exti_lines(). */
 
 /* Sleep mode. Lowest wake-up latency. */
 void power_sleep(void) {
@@ -21,37 +21,33 @@ void power_deepsleep(void) {
 	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
 
 	PWR->CR |= PWR_CR_LPDS; /* Low-power mode ON. */
-	PWR->CR |= PWR_CR_CSBF;
-	PWR->CR |= PWR_CR_CWUF;   /* Clear wake up flag. */
+	PWR->CR |= PWR_CR_CWUF; /* Clear wake up flag. */
 	clear_exti_lines();
 	__WFI();
 } /* End of power_deepsleep(). */
 
-
 /* Standby mode. Lowest amount of power consumption while asleep, but highest wake-up latency. */
-void power_standby(void) {
+void power_standby(uint8_t enableWakeupPin) {
 	SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
 	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-	/* Set PA0 to input. This is the trigger wake up pin. */
-	GPIOA->MODER &= ~(GPIO_MODER_MODER0_Msk);
-	GPIOA->PUPDR |= GPIO_PUPDR_PUPD0_1;
+	PWR->CSR &= ~PWR_CSR_EWUP_Msk; /* Disable wake up wake up pin. */
+	PWR->CR |= PWR_CR_CSBF;        /* Clear standby flag. */
+	PWR->CR |= PWR_CR_CWUF;        /* Clear PWR Wake-up WUF flag. */
+	PWR->CR |= PWR_CR_PDDS;        /* Set PDDS bit to enter standby mode. */
 
-	/* Wait until PA0 is released. */
-	while(GPIOA->IDR & GPIO_IDR_ID0_Msk) { }
+	if(enableWakeupPin) {
+		/* Enable wake up pin. Forces PA0 to input pull down. */
+		PWR->CSR |= PWR_CSR_EWUP;
 
-	/* Temporary fix for preventing switch debounce.
-	 * Hardware implementation would be better alternative.
-	 * */
-	delay_ms(2000);
+		/* Wait until PA0 is released. */
+		while(GPIOA->IDR & GPIO_IDR_ID0_Msk) { }
 
-	PWR->CSR &= ~PWR_CSR_EWUP_Msk;	/* Disable wake up triggers. */
-
-	PWR->CR |= PWR_CR_CSBF;
-	PWR->CR |= PWR_CR_CWUF;   /* Clear wake up flag. */
-	PWR->CR |= PWR_CR_PDDS;	  /* Set PDDS bit to enter standby mode. */
-	PWR->CSR |= PWR_CSR_EWUP; /* Enable wake up triggers. */
+		/* Temporary fix for preventing switch debounce.
+		 * Hardware implementation would be better alternative.
+		 * */
+		delay_ms(2000);
+	}
 
 	/* Ensure no EXTI line is pending. */
 	clear_exti_lines();
@@ -62,13 +58,8 @@ void power_standby(void) {
 /* Clear power-related flags */
 void clear_pwr_flags(void) {
 	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-	if(PWR->CSR & PWR_CSR_SBF) {
-		PWR->CR |= PWR_CR_CSBF; /* Clear standby flag. */
-	}
-	if(PWR->CSR & PWR_CSR_WUF) {
-		PWR->CR |= PWR_CR_CWUF; /* Clear wake up flag. */
-	}
-
+	PWR->CR |= PWR_CR_CSBF; /* Clear standby flag. */
+	PWR->CR |= PWR_CR_CWUF; /* Clear wake up flag. */
 	RCC->APB1ENR &= ~RCC_APB1ENR_PWREN;
-}
+} /* End of clear_pwr_flags(). */
 
