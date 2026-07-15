2db5ff7c10218fbc0a8501a42ec43e1b370a6be7
Marcellus <marcellus.sacramento@gmail.com>
2026-07-15 01:32:22 -0700

    Remove unnecessary checks for clearing flag bits.

--- src/rtc_driver.c
+++ src/rtc_driver.c
@@ -18,7 +18,7 @@ static void enable_rtc_reg_mod(void) {
 	/* Disable write protection of RTC registers. */
 	RTC->WPR = RTC_WPR_KEY_1;
 	RTC->WPR = RTC_WPR_KEY_2;
-}
+} /* End of enable_rtc_reg_mod(). */
 
 static void disable_rtc_reg_mod(void) {
 	uint32_t temp;
@@ -33,8 +33,7 @@ static void disable_rtc_reg_mod(void) {
 	temp = PWR->CR;
 
 	RCC->APB1ENR &= ~RCC_APB1ENR_PWREN;
-}
-
+} /* End of disable_rtc_reg_mod(). */
 
 void rtc_init(void) {
 	/* Turn LSI ON, this clock is used by RTC Wake-up timer. */
@@ -57,13 +56,12 @@ void rtc_init(void) {
 	RCC->BDCR |= RCC_BDCR_RTCEN;
 
 	disable_rtc_reg_mod();
-} /* End of rtc_config(). */
-
+} /* End of rtc_init(). */
 
 
 /* Wake-up timer configuration. Timer resolution is 1 second. From 1s to 36 hours (in seconds). */
-void setRTC_timer(uint32_t timerDuration) {
-	__disable_irq();
+void setWakeupTimer(uint32_t timerDuration) {
+//	__disable_irq();
 	/* If timer duration greater than 36 hours (in seconds), default to max duration. */
 	if(timerDuration > WAKE_UP_TIMER_17_BIT_MAX) {
 		timerDuration = WAKE_UP_TIMER_17_BIT_MAX;
@@ -71,8 +69,7 @@ void setRTC_timer(uint32_t timerDuration) {
 
 	enable_rtc_reg_mod();
 
-	/* Disable wake-up timer. */
-	RTC->CR &= ~RTC_CR_WUTE;
+	RTC->CR &= ~RTC_CR_WUTE; /* Disable wake-up timer. */
 
 	/* Poll until write access to wake-up timer is allowed. */
 	while(!(RTC->ISR & RTC_ISR_WUTWF)) {}
@@ -92,25 +89,19 @@ void setRTC_timer(uint32_t timerDuration) {
 	 * */
 	RTC->WUTR = (timerDuration & WAKE_UP_TIMER_16_BIT_MAX);
 
-	/* Configure wake-up interrupt. */
-	EXTI->IMR |= EXTI_IMR_MR22;
-	EXTI->EMR |= EXTI_EMR_MR22;
-	EXTI->RTSR |= EXTI_RTSR_TR22;   /* Rising-edge. */
+	/* Per datasheet, in case timer is used for waking up device from low-power mode. */
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	RTC->CR &= ~RTC_CR_WUTIE;  /* Disable wake-up interrupt. */
+	RTC->ISR &= ~RTC_ISR_WUTF; /* Ensure flag for wake-up timer is clear. */
+	PWR->CR |= PWR_CR_CWUF;    /* Clear PWR Wake-up WUF flag. */
+	RTC->CR |= RTC_CR_WUTIE;   /* Enable wake-up interrupt. */
+
+	/***** Configure wake-up interrupt. *****/
+	EXTI->PR |= EXTI_PR_PR22;     /* Ensure interrupt line for wake-up timer is clear. */
+	EXTI->IMR |= EXTI_IMR_MR22;   /* Unmask interrupt line 22. */
+	EXTI->EMR |= EXTI_EMR_MR22;   /* Unmask event line 22. */
+	EXTI->RTSR |= EXTI_RTSR_TR22; /* Rising-edge. */
 	NVIC_EnableIRQ(RTC_WKUP_IRQn);
-	RTC->CR |= RTC_CR_WUTIE; 		/* Enable wake-up interrupt. */
-
-	/* Ensure interrupt line for wake-up timer is clear. */
-	if(EXTI->PR & EXTI_PR_PR22) {
-		EXTI->PR |= EXTI_PR_PR22;
-	}
-
-	/* Ensure flag for wake-up timer is clear. */
-	if(RTC->ISR & RTC_ISR_WUTF) {
-		RTC->ISR &= ~RTC_ISR_WUTF;
-	}
-
-	/* Enable interrupts. */
-	__enable_irq();
 
 	/* Start wake-up timer. */
 	RTC->CR |= RTC_CR_WUTE;
@@ -119,16 +110,17 @@ void setRTC_timer(uint32_t timerDuration) {
 	RTC->WPR = RTC_WPR_KEY;
 
 	disable_rtc_reg_mod();
-} /* End of setRTC_timer(). */
+
+	/* Enable interrupts. */
+	__enable_irq();
+} /* End of setWakeupTimer(). */
 
 /* Clears wake-up timer flag. To reload the timer and ensure device go to low-power mode. */
 void clear_rtc_wutf(void) {
 	enable_rtc_reg_mod();
-	if(RTC->ISR & RTC_ISR_WUTF) {
-		RTC->ISR &= ~RTC_ISR_WUTF;
-	}
+	RTC->ISR &= ~RTC_ISR_WUTF;
 	disable_rtc_reg_mod();
-}
+} /* End of clear_rtc_wutf(). */
 
 /* Stop the timer entirely. Timer must be reconfigured again after disabling. */
 void disable_wakeup_timer(void) {
